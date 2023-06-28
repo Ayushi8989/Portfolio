f88cc92053915d29a19ee08aeb0f2f825aea3106
Ayushi <ayushi0628@gmail.com>
2023-06-28 18:27:16 +0530

    update: project-section

--- src/Hero_projects.js
+++ src/Hero_projects.js
@@ -1,22 +1,25 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import project_bg from './images/bg-project.png';
 import './Hero_projects.css';
 import back_button from './images/back_icon.png';
 import pinkz from './images/anim.png'
 import { Link } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.min.js';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function HeroProjects() {
-    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
   const handleMouseMove = (e) => {
     const mouseX = e.clientX;
     const mouseY = e.clientY;
     setTimeout(() => {
-        setMousePosition({ x: mouseX, y: mouseY });
-      }, 10);
+      setMousePosition({ x: mouseX, y: mouseY });
+    }, 10);
   };
 
-  
+
 
   return (
     <div onMouseMove={handleMouseMove}>
@@ -53,28 +56,79 @@ export default function HeroProjects() {
         }}
       ></div>
       <div className='projectsection'>
-            <div className="rounded"></div>
-            <div className="pointed"></div>
-            <div className='cross-button'>
-                <Link to="/"><img src={back_button} alt="back" className="back_button" /></Link>
-            </div>
-            <div className='buildings'>
-                <img src={project_bg} alt="project_bg" className="img-buildings" />
-            </div>
-            <div className="pinkz">
-                <img src={pinkz} alt='pinkz' className='pinkz' />
-            </div>
-            <div className='project-title'>
-                <div className='font-face-gm'>
-                <Link to="/Page"><h3>Orange Horizons</h3></Link>
-                    <h3>Cherry Delight</h3>
-                    <h3>Vaporflow</h3>
-                    <h3>Vitt Kosh</h3>
-                    <h3>Krita</h3>
-                    <h3>Ikshana</h3>
-                    <h3>Lorem ipsue corrupti</h3>
-                </div></div>
+        <div className="rounded"></div>
+        <div className="pointed"></div>
+        <div className='cross-button'>
+          <Link to="/"><img src={back_button} alt="back" className="back_button" /></Link>
         </div>
+        <div className='buildings'>
+          <img src={project_bg} alt="project_bg" className="img-buildings" />
+        </div>
+        <div className="pinkz">
+          <img src={pinkz} alt='pinkz' className='pinkz' />
+        </div>
+        <div className="work">
+          <div className="contents">
+          <li>
+            <div className='font-face-gm'>
+              <h3>
+                <span className='arrow-right'>
+                  <FontAwesomeIcon icon="fa-solid fa-arrow-right" />
+                </span>
+                Vitt Kosh
+              </h3>
+            </div>
+            <h3>UI/UX Design</h3>
+          </li>
+          <li>
+            <div className='font-face-gm'>
+              <h3>
+                <span className='arrow-right'>
+                  <FontAwesomeIcon icon="fa-solid fa-arrow-right" />
+                </span>
+                Krita
+              </h3>
+            </div>
+            <h3>UI/UX Design</h3>
+          </li>
+            <li>
+            <div className='font-face-gm'>
+              <h3>
+                <span className='arrow-right'>
+                  <FontAwesomeIcon icon="fa-solid fa-arrow-right" />
+                </span>
+                Orange Horizons
+              </h3>
+            </div>
+            <h3>Illustration</h3>
+          </li>
+          <li>
+            <div className='font-face-gm'>
+              <h3>
+                <span className='arrow-right'>
+                  <FontAwesomeIcon icon="fa-solid fa-arrow-right" />
+                </span>
+                Cherry Delight
+              </h3>
+            </div>
+            <h3>3D-Modelling</h3>
+          </li>
+          <li>
+            <div className='font-face-gm'>
+              <h3>
+                <span className='arrow-right'>
+                  <FontAwesomeIcon icon="fa-solid fa-arrow-right" />
+                </span>
+                Vaporflow
+              </h3>
+            </div>
+            <h3>Branding</h3>
+          </li>
+          
+          
+          </div>
+        </div>
+    </div >
     </div>
   );
 };
